d28fec0dd3c8cd8aaae8e94aeeae4219e84532c6
Shresth Kapila <49010193+shresthkapila@users.noreply.github.com>
2020-06-04 18:43:35 -0700

    ADDED VALUE 0/1 AND CHECK WITH OUR ANSWER (incomplete)

--- public/main.js
+++ public/main.js
@@ -2,24 +2,54 @@
 
 document.querySelectorAll('.inactive').forEach(function(e) {
     e.addEventListener('click', function(){
+        
+       // console.log(document.getElementsByClassName("inactive").value);
+
         if (this.style.backgroundColor == "white") {
             this.style.backgroundColor = "red";
+            document.getElementsByClassName("inactive").value = "1";
+           // console.log(document.getElementsByClassName("inactive").value);
         }
         else {
             this.style.backgroundColor = "white";
+            document.getElementsByClassName("inactive").value = "0";
+           // console.log(document.getElementsByClassName("inactive").value);
         }
     })
 });
 
 document.querySelectorAll('.active').forEach(function(e) {
     e.addEventListener('click', function(){
+
+       // console.log(document.getElementsByClassName("active").value);
+        
+
         if (this.style.backgroundColor == "white") {
             this.style.backgroundColor = "red";
+            document.getElementsByClassName("active").outerHTML = '<button type= "button" onclick="check()" value="1" class="active">';
+          //  console.log(document.getElementsByClassName("active").value);
         }
         else {
             this.style.backgroundColor = "white";
+            document.getElementsByClassName("active").outerHTML = '<button type= "button" onclick="check()" value="0" class="active">';
+          //  console.log(document.getElementsByClassName("active").value);
         }
     })
 });
 
 
+function check() {
+
+    var arr = document.getElementsByClassName('active');
+
+    for ( var i = 0; i < arr.length; i++) {
+        if (arr[i].value == "1") {
+            console.log("yes");
+
+        }
+        else {
+            console.log("no");
+            console.log(arr[i]);
+        }
+    }
+}
