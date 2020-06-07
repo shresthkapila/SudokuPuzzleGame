66b6c32122a782c18fc6deb0c2ef6155c9d9bada
Shresth Kapila <49010193+shresthkapila@users.noreply.github.com>
2020-06-07 03:09:22 -0700

    directing to html check again

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('public/mypuzzlegame.html'))
+  .get('/', (req, res) => res.sendfile('public/mypuzzlegame.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
